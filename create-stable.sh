5fbebf5b4e612ca198cb4a2dd378036c3f1a5166
veekee <docker@veekee.net>
2016-09-28 11:13:14 +0200

    Switching to safeRun launcher

--- create-stable.sh
+++ create-stable.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -d --name etherpad-stable -e ETHERPAD_ADMIN_USER=admin -e ETHERPAD_ADMIN_PASSWORD=mypassword -v /var/log/etherpad-lite:/var/log/etherpad-lite -v /opt/etherpad-lite/var:/opt/etherpad-lite/var/stable --link=mysql:mysql -p 9001:8080 veekee/rpi-etherpad:stable
+docker run -d --name etherpad-stable -e ETHERPAD_ADMIN_USER=admin -e ETHERPAD_ADMIN_PASSWORD=mypassword -v /var/log/etherpad-lite/stable:/var/log/etherpad-lite -v /opt/etherpad-lite/var/stable:/opt/etherpad-lite/var --link=mysql:mysql -p 9000:9001 veekee/rpi-etherpad:stable
